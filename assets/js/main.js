1d3c35e2a245b6627304cce81ffb4638bbfecff6
Kevin Burke <kburke119@gmail.com>
2025-06-30 14:37:35 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,22 +7,7 @@ $(function() {
     $(window).on('load', function(event) {
         $('.preloader').delay(500).fadeOut(500);
     });
-    
-    
-    //===== Sticky
 
-    $(window).on('scroll', function (event) {
-        var scroll = $(window).scrollTop();
-        if (scroll < 20) {
-            $(".navbar-area").removeClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logo.svg");
-        } else {
-            $(".navbar-area").addClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logo-2.svg");
-        }
-    });
-
-    
     //===== Section Menu Active
 
     var scrollLink = $('.page-scroll');
